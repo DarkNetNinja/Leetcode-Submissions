0c7a2d63bbcfedb8a5b8c7740c37e1bc61c5fe69
Anurag Patil <anuragpatil060@gmail.com>
2026-09-24 11:48:20 +0530

    Time: 50 ms (44.56%) | Memory: 31.1 MB (19.97%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,44 +1,50 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> final;
+        vector<vector<int>> final_vec;
         sort(nums.begin(),nums.end());
-        for(int i{0};i < static_cast<int>(nums.size());i++)
+        for(int i{0};i < static_cast<int>(nums.size())-2;i++)
         {
-            if(i > 0 && nums[i] == nums[i - 1])
+            if(i > 0  && nums[i] == nums[i - 1])
             {
                 continue;
             }
-            
-            int left{i + 1};
-            int right{static_cast<int>(nums.size()) - 1};
-            while(left < right){
-
-            int sum{nums[i] + nums[left] + nums[right]};
-            if(left < right && sum < 0)
+            if(nums[i] > 0)
             {
-                left++;
+                break;
             }
-            else if(left < right && sum > 0)
+            int left {i + 1};
+            int right {static_cast<int>(nums.size()) - 1};
+            int sum{};
+            while(left < right)
             {
-                right--;
-            }
-            else{
-                vector<int> triplets{nums[i],nums[left],nums[right]};
-                final.push_back(triplets);
-                left++;
-                right--;
-                while(left < right && nums[left] == nums[left - 1])
+                sum = nums[i] + nums[left] + nums[right];
+                if(sum < 0)
                 {
                     left++;
                 }
-            while(left < right && nums[right] == nums[right + 1])
+                else if(sum > 0)
                 {
                     right--;
                 }
+                else
+                {
+                    vector<int> triplets{nums[i],nums[left],nums[right]};
+                    final_vec.push_back(triplets);
+                    left++;
+                    right--;
+
+                    while(left < right && nums[left] == nums[left - 1])
+                    {
+                        left++;
+                    }
+                    while(left < right && nums[right] == nums[right + 1])
+                    {
+                        right--;
+                    }
                 }
             }
         }
-        return final;
+        return final_vec;
     }
 };
